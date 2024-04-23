cae421b2ea113e0e765bdc59c6fd9248a36c6bc9
Teodor Engqvist <teodor.engqvist@elits.com>
2024-04-23 17:04:02 +0000

    Added the first label

--- main.tf
+++ main.tf
@@ -54,7 +54,7 @@ resource "google_compute_instance" "hashicat" {
   boot_disk {
     initialize_params {
       image = "ubuntu-os-cloud/ubuntu-2204-lts"
-      lables = {
+      labels = {
         department = "devops"
       }
     }
